34b9f58bbf7c4d3bdb99805cabc0900d64ecf2de
Zahir Bokhari <syedzahirbokhari@gmail.com>
2016-03-16 23:38:27 -0400

    Add data

---
+++ data/quick_model.nvm.cmvs/00/pmvs.sh
@@ -0,0 +1,15 @@
+pmvs2 pmvs/ option-0000
+pmvs2 pmvs/ option-0001
+pmvs2 pmvs/ option-0002
+pmvs2 pmvs/ option-0003
+pmvs2 pmvs/ option-0004
+pmvs2 pmvs/ option-0005
+pmvs2 pmvs/ option-0006
+pmvs2 pmvs/ option-0007
+pmvs2 pmvs/ option-0008
+pmvs2 pmvs/ option-0009
+pmvs2 pmvs/ option-0010
+pmvs2 pmvs/ option-0011
+pmvs2 pmvs/ option-0012
+pmvs2 pmvs/ option-0013
+
